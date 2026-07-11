c25260b7879a5eb35226984e8de4a471a2f3955e
Mayank Ahuja <maahuja123@gmail.com>
2026-07-11 00:29:25 -0600

    Polish Inventory
    Add Valid And Invalid Spots in inventory

--- Survive/Prototype/Assets/Scripts/Inventory/ResourceInventory.cs
+++ Survive/Prototype/Assets/Scripts/Inventory/ResourceInventory.cs
@@ -6,226 +6,255 @@ using UnityEngine.UI;
 
 public class ResourceInventory : MonoBehaviour
 {
-   private int width = 8;
-   private int height = 8;
-   private int occuppiedCount = 0;
-   
- [SerializeField] private float spacingBtwSlots;
-   private InventoryItem heldItem = null;
+    private int width = 8;
+    private int height = 8;
+    private int occuppiedCount = 0;
 
-   [SerializeField] private Slot slotPrefab;
-  
-   private Slot[,] slots;// two dimenstional array can store slots like slots[x,y]= ..
-   private Dictionary<ResourceSo,List< GameObject>> resources = new();
+    [SerializeField] private float spacingBtwSlots;
 
-   private void Awake()
-   {
-      slots = new Slot[width, height];
-      for (int x = 0; x < width; x++)
-      {
-         for (int y = 0; y < height; y++)
-         {
-            var slot = Instantiate(slotPrefab,transform );
-            slot.gridPosition = new Vector2Int(x,y);
-            slot.transform.localPosition = new Vector3(x * spacingBtwSlots, y * spacingBtwSlots , 0);
-            slots[x,y] = slot;
-            Image image = slot.GetComponent<Image>();
-            
-         }
-      }
-   }
-   private void LateUpdate()
-   {
-      if (heldItem == null)return;
-      float depth = Camera.main.WorldToScreenPoint(transform.position).z;
+    private InventoryItem heldItem = null;
 
-         Vector3 worldPos = Camera.main.ScreenToWorldPoint(
-            new Vector3(Input.mousePosition.x, Input.mousePosition.y, depth)
-         );
-         // 2. Convert world → local inventory space
-         Vector3 localPos = transform.InverseTransformPoint(worldPos);
+    [SerializeField] private Slot slotPrefab;
 
-         // 3. Clamp inside inventory bounds
-         float minX = 0;
-         float maxX = (width - 1) * spacingBtwSlots;
-         float minY = 0;
-         float maxY = (height - 1) * spacingBtwSlots;
+    private Slot[,] slots;
+    private Dictionary<ResourceSo, List<GameObject>> resources = new();
 
-         localPos.x = Mathf.Clamp(localPos.x, minX, maxX);
-         localPos.y = Mathf.Clamp(localPos.y, minY, maxY);
 
-         // 4. Apply free movement (no snapping)
-         heldItem.transform.localPosition = localPos;
-   }
-
-   public void TryPlaceItem(ResourceSo So, GameObject itemPrefab)// called by player inventory
-   {
-      Vector2Int size = So.size;
-      for (int x = 0; x < width; x++)
-      {
-         for (int y = 0; y < height; y++)
-         {
-            var slot = slots[x, y];
-            if (IsAreaFree(x,y,size))
+    private void Awake()
+    {
+        slots = new Slot[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
             {
-              PlaceItemAt(itemPrefab, new Vector2Int(x,y), size);
-              if (!resources.ContainsKey(So))
-                 resources[So] = new List<GameObject>();
-
-              resources[So].Add(itemPrefab);
-               return;
+                var slot = Instantiate(slotPrefab, transform);
+                slot.gridPosition = new Vector2Int(x, y);
+                slot.transform.localPosition = new Vector3(x * spacingBtwSlots, y * spacingBtwSlots, 0);
+                slots[x, y] = slot;
             }
-         }
-      }
-   }
-   private bool IsAreaFree(int startX, int startY, Vector2Int size)
-   {
-      // Prevent overflow outside grid
-      if (startX + size.x > width) return false;
-      if (startY + size.y > height) return false;
+        }
+    }
 
-      for (int x = 0; x < size.x; x++)
-      {
-         for (int y = 0; y < size.y; y++)
-         {
-            if (slots[startX + x, startY + y].isOccupied)
-               return false;
-         }
-      }
-      return true;
-   }
-   private void PlaceItemAt(GameObject prefab, Vector2Int position, Vector2Int size)
-   {
-      if(prefab == null){Debug.Log("item is null");}
-      InventoryItem item = prefab.GetComponent<InventoryItem>();
-      
-      occuppiedCount = 0;
-      prefab.transform.SetParent(slots[position.x, position.y].transform);
-      prefab.transform.localPosition = Vector3.zero;
-      prefab.transform.localRotation = Quaternion.identity;
-      prefab.GetComponent<InventoryItem>().origin = position;
-      prefab.GetComponent<InventoryItem>().size = size;
-      
+    private void LateUpdate()
+    {
+        if (heldItem == null) return;
+        UpdateHeldItemPos();
+    }
 
-      slots[position.x, position.y].PlaceItem(prefab);
+    private void UpdateHeldItemPos()
+    {
+        ClearPreviewColors();
+        float depth = Camera.main.WorldToScreenPoint(transform.position).z;
 
-      // Mark all occupied slots
-      for (int x = 0; x < size.x; x++)
-      {
-         for (int y = 0; y < size.y; y++)
-         {
-            slots[position.x + x, position.y + y].isOccupied = true;
-            slots[position.x + x, position.y + y].occupiedItem = item;
-            occuppiedCount++;
-         }
-      }
-   }
-   private void ClearArea(Vector2Int origin, Vector2Int size, InventoryItem item)
-   {
-      for (int x = 0; x < size.x; x++)
-      for (int y = 0; y < size.y; y++)
-      {
-         var s = slots[origin.x + x, origin.y + y];
-         if (s.occupiedItem == item) // only clear if it's the same item
-         {
-            s.isOccupied = false;
-            s.occupiedItem = null;
-         }
-      }
-   }
-   private InventoryItem PickUpItem(Vector2Int gridPos)
-   {
-      Slot slot = slots[gridPos.x, gridPos.y];
-      if (!slot.isOccupied || slot.occupiedItem == null)
-         return null;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, depth)
+        );
+        Vector3 localPos = transform.InverseTransformPoint(worldPos);
 
-      InventoryItem item = slot.occupiedItem;
+        float minX = 0;
+        float maxX = (width - 1) * spacingBtwSlots;
+        float minY = 0;
+        float maxY = (height - 1) * spacingBtwSlots;
 
-      ClearArea(item.origin, item.size, item);
+        localPos.x = Mathf.Clamp(localPos.x, minX, maxX);
+        localPos.y = Mathf.Clamp(localPos.y, minY, maxY);
 
-      return item;
-   }
-   private bool TryPlaceItemAt(InventoryItem item, Vector2Int gridPos)
-   {
-      if (!IsAreaFree(gridPos.x, gridPos.y, item.size))
-         return false;
-
-      PlaceItemAt(item.gameObject, gridPos, item.size);
-      item.origin = gridPos;
-      return true;
-   }
-   public void OnSlotClicked(Slot slot)
-   {
-      Debug.Log("on slot clicked");
-      Vector2Int pos = slot.gridPosition;
+        heldItem.transform.localPosition = localPos;
     
-      if (heldItem == null)
-      {
-         // Pick up item
-         heldItem = PickUpItem(pos);
-         if (heldItem != null)
+        Slot slot = SlotAtCurrentPos(localPos);
+        if (slot == null) return;
+
+        PreviewPlacement(heldItem, slot.gridPosition);
+    }
+    private void ClearPreviewColors()
+    {
+        foreach (var slot in slots)
+            slot.SetRegularColor();
+    }
+    private void PreviewPlacement(InventoryItem item, Vector2Int origin)
+    {
+        Vector2Int size = item.size;
+
+        bool canPlace = IsAreaFree(origin.x, origin.y, size);
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                Slot s = slots[origin.x + x, origin.y + y];
+
+                if (canPlace)
+                    s.Valid();     // turn green
+                else
+                    s.Invalid();   // turn red
+            }
+        }
+    }
+    public void TryPlaceItem(ResourceSo So, GameObject itemPrefab) // called by player inventory
+    {
+        Vector2Int size = So.size;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var slot = slots[x, y];
+                if (IsAreaFree(x, y, size))
+                {
+                    PlaceItemAt(itemPrefab, new Vector2Int(x, y), size);
+                    if (!resources.ContainsKey(So))
+                        resources[So] = new List<GameObject>();
+
+                    resources[So].Add(itemPrefab);
+                    return;
+                }
+            }
+        }
+    }
+
+    private bool IsAreaFree(int startX, int startY, Vector2Int size)
+    {
+        // Prevent overflow outside grid
+        if (startX + size.x > width) return false;
+        if (startY + size.y > height) return false;
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                if (slots[startX + x, startY + y].isOccupied)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void PlaceItemAt(GameObject prefab, Vector2Int position, Vector2Int size)
+    {
+        if (prefab == null)
+        {
+            Debug.Log("item is null");
+        }
+
+        InventoryItem item = prefab.GetComponent<InventoryItem>();
+
+        occuppiedCount = 0;
+        prefab.transform.SetParent(slots[position.x, position.y].transform);
+        prefab.transform.localPosition = Vector3.zero;
+        prefab.transform.localRotation = Quaternion.identity;
+        prefab.GetComponent<InventoryItem>().origin = position;
+        prefab.GetComponent<InventoryItem>().size = size;
+
+
+        slots[position.x, position.y].PlaceItem(prefab);
+
+        // Mark all occupied slots
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                slots[position.x + x, position.y + y].isOccupied = true;
+                slots[position.x + x, position.y + y].occupiedItem = item;
+                occuppiedCount++;
+            }
+        }
+    }
+
+    private void ClearArea(Vector2Int origin, Vector2Int size, InventoryItem item)
+    {
+        for (int x = 0; x < size.x; x++)
+        for (int y = 0; y < size.y; y++)
+        {
+            var s = slots[origin.x + x, origin.y + y];
+            if (s.occupiedItem == item) // only clear if it's the same item
+            {
+                s.isOccupied = false;
+                s.occupiedItem = null;
+            }
+        }
+    }
+
+    private InventoryItem PickUpItem(Vector2Int gridPos)
+    {
+        Debug.Log("pick up item");
+        Slot slot = slots[gridPos.x, gridPos.y];
+        if (!slot.isOccupied || slot.occupiedItem == null)
+            return null;
+
+        InventoryItem item = slot.occupiedItem;
+
+        ClearArea(item.origin, item.size, item);
+
+        return item;
+    }
+
+    private bool CanPlaceItem(InventoryItem item, Vector2Int gridPos)
+    {
+        if (!IsAreaFree(gridPos.x, gridPos.y, item.size))
+            return false;
+
+        return true;
+    }
+
+    public void OnSlotClicked(Slot slot)
+    {
+        Debug.Log("on slot clicked");
+        Vector2Int pos = slot.gridPosition;
+
+        if (heldItem == null)
+        {
+            Debug.Log("heldItem is null");
+            heldItem = PickUpItem(pos);
+            if (heldItem == null) return;
+            Collider collider = heldItem.gameObject.GetComponent<Collider>();
+            if (collider != null)
+                collider.enabled = false;
             heldItem.transform.SetParent(transform); // follow mouse
-      }
-      else
-      {
-         Debug.Log("try place item");
-         // Try to place item
-         if (TryPlaceItemAt(heldItem, pos))
-         {
-            heldItem = null;
-         }
-         else
-         {
-            Debug.Log("Cannot place item here");
-         }
-      }
-   }
+        }
+        else
+        {
+            if (CanPlaceItem(heldItem, pos))
+            {
+                Collider collider = heldItem.GetComponent<Collider>();
+                collider.enabled = true;
+                PlaceItemAt(heldItem.gameObject, pos, heldItem.size);
+                heldItem.origin = pos;
+                heldItem = null;
+                ClearPreviewColors();
+            }
+            else
+            {
+                Debug.Log("Cannot place item here");
+            }
+        }
+    }
 
-   public void RemoveResourse(ResourceSo so)
-   {
-      if (!resources.ContainsKey(so)) return;
-      if (resources[so].Count == 0) return;
+    private Slot SlotAtCurrentPos(Vector3 localPos)
+    {
+        int x = Mathf.RoundToInt(localPos.x / spacingBtwSlots);
+        int y = Mathf.RoundToInt(localPos.y / spacingBtwSlots);
 
-      // Get the first item of this type
-      GameObject item = resources[so][0];
+        if (x < 0 || y < 0 || x >= width || y >= height)
+            return null;
 
-      // Remove from list
-      resources[so].RemoveAt(0);
+        return slots[x, y];
+    }
+    public void RemoveResourse(ResourceSo so)
+    {
+        if (!resources.ContainsKey(so)) return;
+        if (resources[so].Count == 0) return;
 
-      // Destroy the physical object
-     GlobalPool.instance.Return(so.prefab,item);
+        // Get the first item of this type
+        GameObject item = resources[so][0];
 
-      // If list is empty, remove key
-      if (resources[so].Count == 0)
-         resources.Remove(so);
-   }
-   public void DisableAllItemColliders()
-   {
-      Debug.Log("disable all item colliders");
-      foreach (Slot slot in slots)
-      {
-         if (slot.occupiedItem != null)
-         {
-            var col = slot.occupiedItem.GetComponent<Collider>();
-            if (col != null)
-               col.enabled = false;
-         }
-      }
-   }
+        // Remove from list
+        resources[so].RemoveAt(0);
 
-   public void EnableAllItemColliders()
-   {
-      Debug.Log("enable all item colliders");
-      foreach (Slot slot in slots)
-      {
-         if (slot.occupiedItem != null)
-         {
-            var col = slot.occupiedItem.GetComponent<Collider>();
-            if (col != null)
-               col.enabled = true;
-         }
-      }
-   }
+        // Destroy the physical object
+        GlobalPool.instance.Return(so.prefab, item);
 
-
+        // If list is empty, remove key
+        if (resources[so].Count == 0)
+            resources.Remove(so);
+    }
 }
--- Survive/Prototype/Assets/Scripts/Inventory/Slot.cs
+++ Survive/Prototype/Assets/Scripts/Inventory/Slot.cs
@@ -1,26 +1,44 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Slot : MonoBehaviour,IPointerClickHandler
+public class Slot : MonoBehaviour, IPointerClickHandler
 {
-    public Vector2Int gridPosition { get;set; }
-    public bool isOccupied { get;  set; }
-    public Transform itemAnchor;
-    public InventoryItem occupiedItem;
+    public Vector2Int gridPosition { get; set; }
+    public bool isOccupied { get; set; }
+    private Transform itemAnchor;
+    public InventoryItem occupiedItem{get; set;}
 
-  
+    private Image _img;
+    private Color _regularColor;
+    //ToDo : Function for valid and invalid spots 
+
     private void Awake()
     {
         isOccupied = false;
+        _img = GetComponentInChildren<Image>();
+        _regularColor = _img.color;
     }
 
     public void PlaceItem(GameObject item)
     {
+        Debug.Log(" place item");
         item.transform.SetParent(transform);
         item.transform.localPosition = Vector3.zero;
         item.transform.localRotation = Quaternion.identity;
         isOccupied = true;
+    }
+
+    public void Valid()
+    {
+        Debug.Log("Valid");
+        _img.color = Color.green;
+    }
+
+    public void Invalid()
+    {
+        _img.color = Color.red;
     }
 
     public void ClearSlot()
@@ -30,14 +48,22 @@ public class Slot : MonoBehaviour,IPointerClickHandler
 
         isOccupied = false;
     }
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        if(eventData.button != PointerEventData.InputButton.Left)return;
+        if (eventData.button != PointerEventData.InputButton.Left) return;
         ResourceInventory inventory = GetComponentInParent<ResourceInventory>();
-        if(inventory == null){Debug.Log("Cant fint inventory");return;}
+        if (inventory == null)
+        {
+            Debug.Log("Cant find inventory");
+            return;
+        }
+
         inventory.OnSlotClicked(this);
     }
 
+    public void SetRegularColor()
+    {
+        _img.color = _regularColor;
+    }
 }
-
-
