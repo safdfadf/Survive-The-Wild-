68873d2b39dd3c40a87bf6bf0b7c8ed2f391566f
Mayank Ahuja <maahuja123@gmail.com>
2026-07-24 19:49:27 -0600

    Correct formating of input manager

--- Survive/Prototype/Assets/Scripts/Managers/InputManager.cs
+++ Survive/Prototype/Assets/Scripts/Managers/InputManager.cs
@@ -3,37 +3,35 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
-{ 
-       private MovementHandler _player;
-       private PlayerInventory _playerInventory;
-       private PlayerInputs _controls;
-       private CraftingHandler _craftingHandler;
-       private Camera _camera;
-       private PhoneScript _phone;
-       
-     
-       
-       
-       private Action<InputAction.CallbackContext> _movePerformed;
-       private Action<InputAction.CallbackContext> _moveCanceled;
-       private Action<InputAction.CallbackContext> _lookPerformed;
-       private Action<InputAction.CallbackContext> _lookCanceled;
-       private Action<InputAction.CallbackContext> _sprintPerformed;
-       private Action<InputAction.CallbackContext> _sprintCanceled;
-       private Action<InputAction.CallbackContext> _crouchPerformed;
-       private Action<InputAction.CallbackContext> _crouchCanceled;
-       private Action<InputAction.CallbackContext> _jumpPerformed;
-       private Action<InputAction.CallbackContext> _aimStarted;
-       private Action<InputAction.CallbackContext> _aimCanceled;
-       private Action<InputAction.CallbackContext> _shootPerformed;
-       private Action<InputAction.CallbackContext> _cursorToggle;
-       private Action<InputAction.CallbackContext> _inventoryToggle;
-       private Action<InputAction.CallbackContext> _resourceMenuToggle;
-       private Action<InputAction.CallbackContext> _toggleHuntetSenses;
-       private Action<InputAction.CallbackContext> _toggleTarckMenu;
-       private Action<InputAction.CallbackContext> _toggleRecipeBook;
-      
-       private void Awake()
+{
+    private MovementHandler _player;
+    private PlayerInventory _playerInventory;
+    private PlayerInputs _controls;
+    private CraftingHandler _craftingHandler;
+    private Camera _camera;
+    private PhoneScript _phone;
+
+
+    private Action<InputAction.CallbackContext> _movePerformed;
+    private Action<InputAction.CallbackContext> _moveCanceled;
+    private Action<InputAction.CallbackContext> _lookPerformed;
+    private Action<InputAction.CallbackContext> _lookCanceled;
+    private Action<InputAction.CallbackContext> _sprintPerformed;
+    private Action<InputAction.CallbackContext> _sprintCanceled;
+    private Action<InputAction.CallbackContext> _crouchPerformed;
+    private Action<InputAction.CallbackContext> _crouchCanceled;
+    private Action<InputAction.CallbackContext> _jumpPerformed;
+    private Action<InputAction.CallbackContext> _aimStarted;
+    private Action<InputAction.CallbackContext> _aimCanceled;
+    private Action<InputAction.CallbackContext> _shootPerformed;
+    private Action<InputAction.CallbackContext> _cursorToggle;
+    private Action<InputAction.CallbackContext> _inventoryToggle;
+    private Action<InputAction.CallbackContext> _resourceMenuToggle;
+    private Action<InputAction.CallbackContext> _toggleHuntetSenses;
+    private Action<InputAction.CallbackContext> _toggleTarckMenu;
+    private Action<InputAction.CallbackContext> _toggleRecipeBook;
+
+    private void Awake()
     {
         _camera = Camera.main;
         _player = GetComponent<MovementHandler>();
@@ -61,43 +59,39 @@ public class InputManager : MonoBehaviour
 
         _sprintPerformed = ctx => _player._isSprinting = true;
         _sprintCanceled = ctx => _player._isSprinting = false;
-        
+
         _crouchPerformed = ctx => _player.IsCrouching = true;
         _crouchCanceled = ctx => _player.IsCrouching = false;
 
         _jumpPerformed = ctx => _player.Jump();
         _toggleHuntetSenses = ctx => _player.ToggleHunterSense();
         _toggleRecipeBook = ctx => _craftingHandler.ToggleRBook();
-        
-        
 
-        _aimStarted = ctx => {
+
+        _aimStarted = ctx =>
+        {
             var weapon = _player.CurrentWeapon;
             if (weapon != null && weapon.isAimable)
                 weapon.StartAiming();
         };
 
-        _aimCanceled = ctx => {
+        _aimCanceled = ctx =>
+        {
             var weapon = _player.CurrentWeapon;
             if (weapon != null && weapon.isAimable)
                 weapon.StopAiming();
         };
-        _shootPerformed = ctx =>  
+        _shootPerformed = ctx =>
         {
             var weapon = _player.CurrentWeapon;
-            
-            if (weapon != null )
+
+            if (weapon != null)
             {
                 EventBus.onAttack.Invoke();
             }
-           
         };
         _cursorToggle = ctx => _player.ToggleCursor();
-        _inventoryToggle = ctx =>
-        {
-            _player.ToggleInventory();
-            _craftingHandler.ToggleCrafting();
-        };
+        _inventoryToggle = ctx => { _player.ToggleInventory(); };
         _resourceMenuToggle = ctx => ToggleCollectableMenu();
         _toggleTarckMenu = ctx => ToggleTracksMenu();
     }
@@ -130,7 +124,7 @@ public class InputManager : MonoBehaviour
         _controls.PlayerInteract.Inventory.performed += _inventoryToggle;
         _controls.PlayerInteract.ResourceMenu.performed += _resourceMenuToggle;
         _controls.PlayerInteract.Interact.performed += _toggleTarckMenu;
-        _controls.PlayerInteract.Phone.started += ctx=> _phone.MoveInPhone();
+        _controls.PlayerInteract.Phone.started += ctx => _phone.MoveInPhone();
         _controls.PlayerInteract.Phone.canceled += ctx => _phone.MoveOutPhone();
         _controls.PlayerInteract.Scroll.performed += _phone.Scroll;
         _controls.PlayerInteract.Book.performed += _toggleRecipeBook;
@@ -156,15 +150,16 @@ public class InputManager : MonoBehaviour
         _controls.PlayerInteract.Inventory.performed -= _inventoryToggle;
         _controls.PlayerInteract.ResourceMenu.performed -= _resourceMenuToggle;
         _controls.PlayerMovement.HunerSense.performed -= _toggleHuntetSenses;
-        _controls.PlayerInteract.Interact.performed  -= _toggleTarckMenu;
+        _controls.PlayerInteract.Interact.performed -= _toggleTarckMenu;
         _controls.PlayerInteract.Book.performed -= _toggleRecipeBook;
         _controls.Disable();
     }
+
     private void ToggleCollectableMenu()
     {
         Ray ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
         int mask = LayerMask.GetMask("Resources");
-        if (Physics.SphereCast(ray,.5f,out RaycastHit hit, 3f,mask))
+        if (Physics.SphereCast(ray, .5f, out RaycastHit hit, 3f, mask))
         {
             ICollectable collectable = hit.collider.GetComponent<ICollectable>();
             if (collectable != null)
@@ -178,9 +173,8 @@ public class InputManager : MonoBehaviour
     {
         EventBus.OnToggleTracksMenu?.Invoke();
         _playerInventory.SubmitResource();
-        
     }
-    
+
     private void OnDestroy()
     {
         _controls.Disable();
